e3643870af71cca7abae6e4cbcc82e9bc877e75f
Daniel King <daniel@allenai.org>
2019-09-05 15:44:51 -0700

    Add back confidences and adjust default params

--- sequential_sentence_classification/dataset_reader.py
+++ sequential_sentence_classification/dataset_reader.py
@@ -75,6 +75,8 @@ class SeqClassificationReader(DatasetReader):
         else:
             labels = None
 
+        confidences = json_dict.get("confs", None)
+
         additional_features = None
         if self.sci_sum:
             if self.sci_sum_fake_scores:
@@ -98,18 +100,19 @@ class SeqClassificationReader(DatasetReader):
             if len(sentences) == 0:
                 return []
 
-        for sentences_loop, labels_loop, additional_features_loop in  \
-                self.enforce_max_sent_per_example(sentences, labels, additional_features):
+        for sentences_loop, labels_loop, confidences_loop, additional_features_loop in  \
+                self.enforce_max_sent_per_example(sentences, labels, confidences, additional_features):
 
             instance = self.text_to_instance(
                 sentences=sentences_loop,
                 labels=labels_loop,
+                confidences=confidences_loop,
                 additional_features=additional_features_loop,
                 )
             instances.append(instance)
         return instances
 
-    def enforce_max_sent_per_example(self, sentences, labels=None, additional_features=None):
+    def enforce_max_sent_per_example(self, sentences, labels=None, confidences=None, additional_features=None):
         """
         Splits examples with len(sentences) > self.max_sent_per_example into multiple smaller examples
         with len(sentences) <= self.max_sent_per_example.
@@ -121,6 +124,8 @@ class SeqClassificationReader(DatasetReader):
         """
         if labels is not None:
             assert len(sentences) == len(labels)
+        if confidences is not None:
+            assert len(sentences) == len(confidences)
         if additional_features is not None:
             assert len(sentences) == len(additional_features)
 
@@ -128,13 +133,15 @@ class SeqClassificationReader(DatasetReader):
             i = len(sentences) // 2
             l1 = self.enforce_max_sent_per_example(
                     sentences[:i], None if labels is None else labels[:i],
+                    None if confidences is None else confidences[:i],
                     None if additional_features is None else additional_features[:i])
             l2 = self.enforce_max_sent_per_example(
                     sentences[i:], None if labels is None else labels[i:],
+                    None if confidences is None else confidences[i:],
                     None if additional_features is None else additional_features[i:])
             return l1 + l2
         else:
-            return [(sentences, labels, additional_features)]
+            return [(sentences, labels, confidences, additional_features)]
 
     def is_bad_sentence(self, sentence: str):
         if len(sentence) > 10 and len(sentence) < 600:
@@ -171,10 +178,13 @@ class SeqClassificationReader(DatasetReader):
     def text_to_instance(self,
                          sentences: List[str],
                          labels: List[str] = None,
+                         confidences: List[float] = None,
                          additional_features: List[float] = None,
                          ) -> Instance:
         if not self.predict:
             assert len(sentences) == len(labels)
+        if confidences is not None:
+            assert len(sentences) == len(confidences)
         if additional_features is not None:
             assert len(sentences) == len(additional_features)
 
@@ -209,6 +219,8 @@ class SeqClassificationReader(DatasetReader):
                             LabelField(str(label)+"_label") for label in labels
                         ])
 
+        if confidences is not None:
+            fields['confidences'] = ArrayField(np.array(confidences))
         if additional_features is not None:
             fields["additional_features"] = ArrayField(np.array(additional_features))
 
